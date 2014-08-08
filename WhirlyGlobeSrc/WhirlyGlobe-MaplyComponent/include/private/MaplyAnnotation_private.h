bb761dc9bb645334eeef04ecf857751c8950bd3d
Suzanne Kiihne <suz@gaiagps.com>
2014-08-07 18:44:42 -0700

    make Annotation.loc public

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/private/MaplyAnnotation_private.h
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/private/MaplyAnnotation_private.h
@@ -20,11 +20,9 @@
 
 #import "MaplyAnnotation.h"
 #import "SMCalloutView.h"
-#import "MaplyCoordinate.h"
 
 @interface MaplyAnnotation()
 
 @property (nonatomic) SMCalloutView *calloutView;
-@property (nonatomic) MaplyCoordinate loc;
 
 @end
