ef9a2af4bf9e526374d6c48547d9d12a04186381
Jesse Crocker <jesse@gaiagps.com>
2015-11-19 12:43:15 -0700

    add removeShadeProgram: method to MaplyBaseViewController

--- WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyBaseViewController.h
+++ WhirlyGlobeSrc/WhirlyGlobe-MaplyComponent/include/MaplyBaseViewController.h
@@ -833,6 +833,10 @@ typedef enum {MaplyThreadCurrent,MaplyThreadAny} MaplyThreadMode;
   */
 - (void)addShaderProgram:(MaplyShader *)shader sceneName:(NSString *)sceneName;
 
+/** @brief Removed a previously added shader.
+ */
+- (void)removeShaderProgram:(MaplyShader *)shader;
+
 /** @brief Look for a shader with the given name.  
     @details This is the shader's own name as specified in the init call, not the scene name as might be specified in addShaderProgram:sceneName:
     @return Returns the registered shader if it found one.
